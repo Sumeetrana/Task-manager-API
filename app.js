0deb53f35c48b71b034c7d54dcf13f9749ea1d86
Sumeetrana <sumeetsinh28@gmail.com>
2022-06-28 11:30:14 +0530

    creating route and controller for getting all tasks

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const taskRoutes = require("./routes/tasks");
 
 const port = 5000;
 
+// middleware
+app.use(express.json())
+
+// routes
 app.get("/hello", (req, res) => {
     res.send("Hello")
 })
+
+app.use('/api/v1/tasks', taskRoutes)
 
 app.listen(port, () => {
     console.log("Server is listening....");
