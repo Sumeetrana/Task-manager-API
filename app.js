5d36cf332ed0c3048ac4dad8eb16897b86c915b8
Sumeetrana <sumeetsinh28@gmail.com>
2022-06-28 11:18:43 +0530

    setting up server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/hello", (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(port, () => {
+    console.log("Server is listening....");
+})
